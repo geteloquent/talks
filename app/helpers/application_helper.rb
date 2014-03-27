9b7b41e9df51684be618c3f35e92f39a53b0b739
filipewl <fwl.ufpe@gmail.com>
2014-03-27 09:16:13 -0300

    Remove helper não utilizado.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,5 @@
 module ApplicationHelper
 
-  def pluralize_without_counter(counter, singular, plural = nil)
-    pluralize(counter, singular, plural).gsub(/^\d+\s/, "")
-  end
-
   def errors_for(object, method)
     errors = object.errors[method].collect do |msg|
       content_tag(:li, msg)
