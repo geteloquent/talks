556513df5b7eee626aeedcddec481ca3174aa07e
filipewl <fwl.ufpe@gmail.com>
2014-04-15 16:51:29 -0300

    Adiciona specs para ordenação de talks por deadline (guiocavalcanti)

--- spec/controllers/talks_controller_spec.rb
+++ spec/controllers/talks_controller_spec.rb
@@ -1,6 +1,21 @@
 require "spec_helper"
 
 describe TalksController do
+  describe "GET #index" do
+    let(:talks) do
+      3.times.map { |i| create(:talk, deadline: Date.today + i.days) }
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assings all talks as @talks" do
+      get :index
+      expect(assigns(:talks)).to eq(talks)
+    end
+  end
 
   describe "GET #show" do
     let(:talk) { FactoryGirl.create(:talk) }
