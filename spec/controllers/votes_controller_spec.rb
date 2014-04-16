116d69b405f31d03774f30f844db4c7f90460d24
filipewl <fwl.ufpe@gmail.com>
2014-04-16 13:37:41 -0300

    Remove um exemplo desnecessário no spec de VotesController.

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -5,11 +5,6 @@ describe VotesController do
     let(:talk) { create(:talk) }
     let(:vote) { "dislike" }
 
-    it "invokes TalkVoting#vote" do
-      expect_any_instance_of(TalkVoting).to receive(:vote).with(User.anonymous, vote)
-      post :create, { talk_id: talk.id, vote: vote }
-    end
-
     it "redirects back to the talk#show" do
       post :create, { talk_id: talk.id, vote: vote }
       expect(response).to redirect_to(talk)
