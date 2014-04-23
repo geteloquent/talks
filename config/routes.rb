5efb0ed52959b18c53f29f39f2b55e439eda9d58
filipewl <fwl.ufpe@gmail.com>
2014-04-23 10:55:59 -0300

    Seta o root para talks#index ao invés de fazer redirecionamento.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Elotalks::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  get '/', to: redirect('/talks')
+  root 'talks#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
